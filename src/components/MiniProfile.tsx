55be9e95b69a9cc5f19073cf9490de5a50128ceb
patsicko <patsicko@gmail.com>
2024-04-22 16:30:33 +0200

    complete main profile component

--- src/components/MiniProfile.tsx
+++ src/components/MiniProfile.tsx
@@ -8,10 +8,18 @@ function MiniProfile() {
 
     const {data:session} = useSession()
 
+    const handleSignIn = () => {
+        signIn();
+    };
+
+    const handleSignOut = () => {
+        signOut();
+    };
+
   return (
     <div className='flex items-center justify-between mt-14 scroll-ml-10'>
      <img src={session?.user?.image || '/packlogo.png' } alt="user img" className=' w-16 h-16 rounded-full border p-[2px]'/>
-     <div>
+     <div className='flex-1 ml-4'>
         <h2 className='font-bold'>
          {session?.user && (session.user as CustomUser).username}
         </h2>
@@ -21,6 +29,10 @@ function MiniProfile() {
         </h3>
 
      </div>
+
+     {session ? (
+        <button onClick={handleSignOut} className='text-blue-500 text-sm font-semi-bold'>Signout</button>
+     ):(<button onClick={handleSignIn} className='text-blue-500 text-sm font-semi-bold'>Signin</button>)}
     </div>
   )
 }
