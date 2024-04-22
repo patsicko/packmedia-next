9f37ab6a8f0c6248e9b880983db95b1d4d4f688e
patsicko <patsicko@gmail.com>
2024-04-22 13:42:44 +0200

    displaying MiniProfile

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -10,7 +10,10 @@ function Feed() {
     </section>
 
     <section className='hidden md:inline-grid md:col-span-1'>
-  <MiniProfile/>
+        <div className='fixed w-[380px]'>
+        <MiniProfile/>
+        </div>
+  
     </section>
     </main>
   )
--- src/components/MiniProfile.tsx
+++ src/components/MiniProfile.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { useSession,signIn,signOut } from 'next-auth/react';
+import { CustomUser } from '@/app/api/auth/[...nextauth]/route';
+
 
 function MiniProfile() {
+
+    const {data:session} = useSession()
+
   return (
-    <div>
-      MiniProfile
+    <div className='flex items-center justify-between mt-14 scroll-ml-10'>
+     <img src={session?.user?.image || '/packlogo.png' } alt="user img" className=' w-16 h-16 rounded-full border p-[2px]'/>
+     <div>
+        <h2 className='font-bold'>
+         {session?.user && (session.user as CustomUser).username}
+        </h2>
+
+        <h3 className='text-sm text-gray-400'>
+         Welcome to PackMedia
+        </h3>
+
+     </div>
     </div>
   )
 }
