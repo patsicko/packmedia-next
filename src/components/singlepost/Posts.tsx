dfa157a5663eccda377749ab150ddceaee210def
king sharoon Bizimana <139285398+brwanda7@users.noreply.github.com>
2024-05-02 15:44:11 +0200

    Timestamp

--- src/components/singlepost/Posts.tsx
+++ src/components/singlepost/Posts.tsx
@@ -1,56 +1,65 @@
-
 import { app } from '@/firebase';
-import { collection, getDocs, getFirestore, orderBy, query, where } from 'firebase/firestore'
-import React from 'react'
+import { collection, getDocs, getFirestore, orderBy, query, where , Timestamp } from 'firebase/firestore';
+import React from 'react';
 
 type Post = {
-  id:string;
-  image:string;
-  caption:string;
-  username:string;
-  timestamp:Timestamp;
-  profileImg:string;
+  id: string;
+  image: string;
+  caption: string;
+  username: string;
+  timestamp: Timestamp;
+  profileImg: string;
 }
 
 type Props = {
   id: string;
 }
 
-const Page: React.FC<Props> = ({ id }) => {
-  const [data, setData] = React.useState<Post[]>([]);
-  
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const db = getFirestore(app);
-        const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'), where('id', '==', id));
-        const querySnapshot = await getDocs(q);
-  
-        const postData: Post[] = [];
-  
-        querySnapshot.forEach((doc) => {
-          postData.push({ id: doc.id, ...doc.data() } as Post);
-        });
-  
-        setData(postData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+class Page extends React.Component<Props> {
+  state = {
+    data: [] as Post[]
+  };
 
-    fetchData();
-  }, [id]); // Add id as a dependency to useEffect
-  
-  return (
-    <div>
-      {data.map((post) => (
-        <div  key={post.id}>
-        <div>{post.id}</div>
-        <span className='font-bold mr-2'>{post.username}</span>
-        </div>
-      ))}
-    </div>
-  );
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.id !== this.props.id) {
+      this.fetchData();
+    }
+  }
+
+  fetchData = async () => {
+    try {
+      const db = getFirestore(app);
+      const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'), where('id', '==', this.props.id));
+      const querySnapshot = await getDocs(q);
+
+      const postData: Post[] = [];
+
+      querySnapshot.forEach((doc) => {
+        postData.push({ id: doc.id, ...doc.data() } as Post);
+      });
+
+      this.setState({ data: postData });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.data.map((post) => (
+          <div key={post.id}>
+            <div>{post.id}</div>
+            <span className='font-bold mr-2'>{post.username}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Page;
